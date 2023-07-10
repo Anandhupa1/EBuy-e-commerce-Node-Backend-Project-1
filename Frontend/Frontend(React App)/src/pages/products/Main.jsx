5b5a6cf8cdf51a4f3b29de1c77f5c31834f0438c
Anandhu_P_A <anandhupa131@gmail.com>
2023-07-10 22:27:55 +0530

    empty data illustration added in product page

--- Frontend/Frontend(React App)/src/pages/products/Main.jsx
+++ Frontend/Frontend(React App)/src/pages/products/Main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Card from "./Product"
-import { GridItem,Box ,SimpleGrid} from '@chakra-ui/react'
+import { GridItem,Box ,SimpleGrid,Image} from '@chakra-ui/react'
 import { useSelector } from 'react-redux';
-
+import emptyImage from "../../assets/images/empty1.png"
 function Main() {
   let count =0;
   const cart = useSelector((store)=>store.cart.cart);
   return (
     <GridItem borderRadius={"10px"} className='boxShadow' p='4'  area={'main'}
     >
+ {
+   cart.length!==0?(
+      
     <SimpleGrid 
     columns={4} 
      alignContent={"flex-start"}
@@ -17,19 +20,24 @@ function Main() {
      width={"100%"}
      height={"100%"}  >
   
-   {
-   
+  
+    {
     cart.map(item => {
       count++;
       return <Card item={item}/>
     })
-    
+     }
+    </SimpleGrid>
+   )
+   :
+   
+   <Image marginLeft={"200px"} marginTop={"100px"} width={"400px"} height={"400px"} src={emptyImage} />
   
    }
     
 
     
-    </SimpleGrid>
+  
 
 
 
